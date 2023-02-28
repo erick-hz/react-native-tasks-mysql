99fc2f4a9fe73f3f1b16850ec2ea1e0c4e3ee228
Erick Hernandez <yerickk8@gmail.com>
2023-02-28 08:58:05 -0600

    Setup endpoints

--- backend/src/controllers/tasks.js
+++ backend/src/controllers/tasks.js
@@ -3,26 +3,45 @@ import { connect } from "../database";
 export const getTasks = async (req, res) => {
     const connection = await connect();
     const [rows] = await connection.execute("SELECT * FROM tasks");
-    console.log(rows);
-    res.json("hello world");
+    res.json(rows);
 }
 
-export const getTask = (req, res) => {
-    res.json("hello world");
+export const getTask = async (req, res) => {
+    const connection = await connect();
+    const [rows] = await connection.execute("SELECT * FROM tasks WHERE id = ?", [req.params.id]);
+    res.json(rows[0]);
 }
 
-export const getTasksCount = (req, res) => {
-    res.json("hello world");
+export const getTasksCount = async (req, res) => {
+    const connection = await connect();
+    const [rows] = await connection.execute("SELECT COUNT(*) FROM tasks");
+    res.json(rows[0]["COUNT(*)"]);
 }
 
-export const saveTasks = (req, res) => {
-    res.json("hello world");
+export const saveTasks = async (req, res) => {
+    const connection = await connect();
+    const [result] = await connection.query("INSERT INTO tasks(title, description) VALUES (?,?)", [
+        req.body.title,
+        req.body.description
+    ]);
+
+    res.json({
+        id: result.insertId,
+        ...req.body
+    });
 }
 
-export const deleteTasks = (req, res) => {
-    res.json("hello world");
+export const deleteTasks = async (req, res) => {
+    const connection = await connect();
+    await connection.query("DELETE FROM tasks WHERE id = ?", [req.params.id]);
+    res.sendStatus(204);
 }
 
-export const updateTasks = (req, res) => {
-    res.json("hello world");
+export const updateTasks = async (req, res) => {
+    const connection = await connect();
+    const results = await connection.query("UPDATE tasks SET ? WHERE id = ?", [
+        req.body,
+        req.params.id
+    ]);
+    res.sendStatus(204);
 }
